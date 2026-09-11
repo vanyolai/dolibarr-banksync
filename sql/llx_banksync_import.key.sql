ae0835a123704f434302e53d99d6de667a7362d1
Vanyo <vanyolai@gmail.com>
2026-09-11 23:08:35 +0200

    Index import source account links

--- sql/llx_banksync_import.key.sql
+++ sql/llx_banksync_import.key.sql
@@ -3,3 +3,4 @@
 ALTER TABLE llx_banksync_import ADD UNIQUE INDEX uk_banksync_import_source (entity, provider, source_sha256);
 ALTER TABLE llx_banksync_import ADD INDEX idx_banksync_import_account (entity, account_number);
 ALTER TABLE llx_banksync_import ADD INDEX idx_banksync_import_period (period_start, period_end);
+ALTER TABLE llx_banksync_import ADD INDEX idx_banksync_import_source_account (fk_banksync_account);
