febcd9f230bea404682c7f9fa758b6b6276a52e4
Vanyo <vanyolai@gmail.com>
2026-09-16 13:40:20 +0200

    Allow multiple tax types per destination account

--- sql/llx_banksync_tax_account_map.key.sql
+++ sql/llx_banksync_tax_account_map.key.sql
@@ -1,2 +1,2 @@
-ALTER TABLE llx_banksync_tax_account_map ADD UNIQUE INDEX uk_banksync_tax_account (entity, target_account_number);
+ALTER TABLE llx_banksync_tax_account_map ADD UNIQUE INDEX uk_banksync_tax_account_type (entity, target_account_number, fk_charge_type);
 ALTER TABLE llx_banksync_tax_account_map ADD INDEX idx_banksync_tax_charge_type (fk_charge_type);
