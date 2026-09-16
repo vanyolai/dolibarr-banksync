03c8792cf01515946ce8423975892f7d087a6fea
Vanyo <vanyolai@gmail.com>
2026-09-16 15:28:08 +0200

    Separate VAT and social tax account mappings

--- admin/taxaccounts.php
+++ admin/taxaccounts.php
@@ -10,6 +10,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
 require_once __DIR__.'/../lib/banksync.lib.php';
 require_once __DIR__.'/../class/banksyncschema.class.php';
 require_once __DIR__.'/../class/banksynctaxaccountmanager.class.php';
+require_once __DIR__.'/../class/banksyncvataccountmanager.class.php';
 
 $langs->load('banksync@banksync');
 if (!$user->admin) accessforbidden();
@@ -21,6 +22,7 @@ try {
 }
 
 $manager = new BankSyncTaxAccountManager($db, (int) $conf->entity);
+$vatManager = new BankSyncVatAccountManager($db, (int) $conf->entity);
 $action = GETPOST('action', 'aZ09');
 
 if ($action === 'save') {
@@ -44,12 +46,34 @@ if ($action === 'delete') {
         setEventMessages($langs->trans('BankSyncTaxAccountMappingFailed', $e->getMessage()), null, 'errors');
     }
 }
+if ($action === 'save_vat') {
+    try {
+        $vatManager->save(
+            GETPOST('vat_target_account_number', 'alphanohtml'),
+            GETPOST('vat_mapping_label', 'alphanohtml'),
+            (int) $user->id
+        );
+        setEventMessages($langs->trans('BankSyncVatAccountMappingSaved'), null, 'mesgs');
+    } catch (Throwable $e) {
+        setEventMessages($langs->trans('BankSyncVatAccountMappingFailed', $e->getMessage()), null, 'errors');
+    }
+}
+if ($action === 'delete_vat') {
+    try {
+        $vatManager->delete(GETPOSTINT('id'));
+        setEventMessages($langs->trans('BankSyncVatAccountMappingDeleted'), null, 'mesgs');
+    } catch (Throwable $e) {
+        setEventMessages($langs->trans('BankSyncVatAccountMappingFailed', $e->getMessage()), null, 'errors');
+    }
+}
 
 $mappings = array();
+$vatMappings = array();
 $types = array();
 $observed = array();
 try {
     $mappings = $manager->getMappings();
+    $vatMappings = $vatManager->getMappings();
     $types = $manager->getChargeTypes();
     $observed = $manager->getUnmappedObservedAccounts(30);
 } catch (Throwable $e) {
@@ -58,6 +82,11 @@ try {
 
 $prefillAccount = GETPOST('prefill_account', 'alphanohtml');
 $prefillLabel = GETPOST('prefill_label', 'alphanohtml');
+$prefillKind = GETPOST('prefill_kind', 'aZ09');
+$prefillVatAccount = $prefillKind === 'vat' ? $prefillAccount : '';
+$prefillVatLabel = $prefillKind === 'vat' ? $prefillLabel : '';
+$prefillSocialAccount = $prefillKind === 'vat' ? '' : $prefillAccount;
+$prefillSocialLabel = $prefillKind === 'vat' ? '' : $prefillLabel;
 
 llxHeader('', $langs->trans('BankSyncTaxAccountMappings'));
 $head = banksyncAdminPrepareHead();
@@ -65,12 +94,14 @@ print dol_get_fiche_head($head, 'taxaccounts', $langs->trans('BankSync'), -1, 'b
 
 print '<div class="opacitymedium">'.$langs->trans('BankSyncTaxAccountMappingsHelp').'</div><br>';
 
+print load_fiche_titre($langs->trans('BankSyncSocialTaxMappings'), '', 'payment');
+print '<div class="opacitymedium">'.$langs->trans('BankSyncSocialTaxMappingsHelp').'</div><br>';
 print '<form method="POST" action="'.dol_escape_htmltag($_SERVER['PHP_SELF']).'">';
 print '<input type="hidden" name="token" value="'.newToken().'">';
 print '<input type="hidden" name="action" value="save">';
 print '<table class="border centpercent">';
 print '<tr><td class="titlefield fieldrequired">'.$langs->trans('BankSyncTaxDestinationAccount').'</td>';
-print '<td><input type="text" class="minwidth300" name="target_account_number" value="'.dol_escape_htmltag($prefillAccount).'" placeholder="10032000..."> <span class="opacitymedium">'.$langs->trans('BankSyncTaxDestinationAccountHelp').'</span></td></tr>';
+print '<td><input type="text" class="minwidth300" name="target_account_number" value="'.dol_escape_htmltag($prefillSocialAccount).'" placeholder="10032000..."> <span class="opacitymedium">'.$langs->trans('BankSyncTaxDestinationAccountHelp').'</span></td></tr>';
 print '<tr><td class="fieldrequired">'.$langs->trans('BankSyncTaxContributionType').'</td><td>';
 print '<select name="fk_charge_type" class="minwidth300">';
 print '<option value="0">'.$langs->trans('BankSyncSelectTaxContributionType').'</option>';
@@ -80,7 +111,7 @@ foreach ($types as $type) {
     print '<option value="'.((int) $type->id).'">'.dol_escape_htmltag($label).'</option>';
 }
 print '</select></td></tr>';
-print '<tr><td>'.$langs->trans('Label').'</td><td><input type="text" class="minwidth300" name="mapping_label" value="'.dol_escape_htmltag($prefillLabel).'" placeholder="NAV TB / NAV SZJA / NAV Szocho"></td></tr>';
+print '<tr><td>'.$langs->trans('Label').'</td><td><input type="text" class="minwidth300" name="mapping_label" value="'.dol_escape_htmltag($prefillSocialLabel).'" placeholder="NAV TB / NAV SZJA / NAV Szocho"></td></tr>';
 print '</table>';
 print '<div class="center"><button type="submit" class="button button-save">'.$langs->trans('BankSyncAddOrUpdateMapping').'</button></div>';
 print '</form>';
@@ -106,17 +137,52 @@ if (empty($mappings)) {
 }
 print '</table></div>';
 
+print '<br><a id="vat-mapping"></a>'.load_fiche_titre($langs->trans('BankSyncVatMappings'), '', 'payment');
+print '<div class="opacitymedium">'.$langs->trans('BankSyncVatMappingsHelp').'</div><br>';
+print '<form method="POST" action="'.dol_escape_htmltag($_SERVER['PHP_SELF']).'#vat-mapping">';
+print '<input type="hidden" name="token" value="'.newToken().'">';
+print '<input type="hidden" name="action" value="save_vat">';
+print '<table class="border centpercent">';
+print '<tr><td class="titlefield fieldrequired">'.$langs->trans('BankSyncTaxDestinationAccount').'</td>';
+print '<td><input type="text" class="minwidth300" name="vat_target_account_number" value="'.dol_escape_htmltag($prefillVatAccount).'" placeholder="10032000..."> <span class="opacitymedium">'.$langs->trans('BankSyncVatDestinationAccountHelp').'</span></td></tr>';
+print '<tr><td>'.$langs->trans('Label').'</td><td><input type="text" class="minwidth300" name="vat_mapping_label" value="'.dol_escape_htmltag($prefillVatLabel).'" placeholder="NAV ÁFA"></td></tr>';
+print '</table>';
+print '<div class="center"><button type="submit" class="button button-save">'.$langs->trans('BankSyncAddOrUpdateMapping').'</button></div>';
+print '</form>';
+
+print '<br>'.load_fiche_titre($langs->trans('BankSyncConfiguredVatAccounts'), '', 'bank');
+print '<div class="div-table-responsive"><table class="noborder centpercent">';
+print '<tr class="liste_titre"><th>'.$langs->trans('BankSyncTaxDestinationAccount').'</th><th>'.$langs->trans('Label').'</th><th class="center">'.$langs->trans('Status').'</th><th class="right">'.$langs->trans('Action').'</th></tr>';
+if (empty($vatMappings)) {
+    print '<tr><td colspan="4"><span class="opacitymedium">'.$langs->trans('BankSyncNoVatAccountMappings').'</span></td></tr>';
+} else {
+    foreach ($vatMappings as $mapping) {
+        print '<tr class="oddeven">';
+        print '<td><strong>'.dol_escape_htmltag((string) $mapping->target_account_number).'</strong></td>';
+        print '<td>'.dol_escape_htmltag((string) $mapping->label).'</td>';
+        print '<td class="center"><span class="badge badge-status4">'.$langs->trans('Enabled').'</span></td>';
+        $deleteUrl = $_SERVER['PHP_SELF'].'?action=delete_vat&id='.(int) $mapping->rowid.'&token='.newToken().'#vat-mapping';
+        print '<td class="right"><a class="buttonDelete" href="'.dol_escape_htmltag($deleteUrl).'">'.$langs->trans('Delete').'</a></td>';
+        print '</tr>';
+    }
+}
+print '</table></div>';
+
 print '<br>'.load_fiche_titre($langs->trans('BankSyncObservedTaxAccounts'), '', 'search');
 print '<div class="opacitymedium">'.$langs->trans('BankSyncObservedTaxAccountsHelp').'</div>';
 print '<div class="div-table-responsive"><table class="noborder centpercent">';
-print '<tr class="liste_titre"><th>'.$langs->trans('BankSyncTaxDestinationAccount').'</th><th>'.$langs->trans('BankSyncCounterparty').'</th><th>'.$langs->trans('BankSyncLastSeen').'</th><th class="right">'.$langs->trans('BankSyncTransactionCountShort').'</th><th class="right">'.$langs->trans('Action').'</th></tr>';
+print '<tr class="liste_titre"><th>'.$langs->trans('BankSyncTaxDestinationAccount').'</th><th>'.$langs->trans('BankSyncCounterparty').'</th><th>'.$langs->trans('BankSyncSuggestedTargetKind').'</th><th>'.$langs->trans('BankSyncLastSeen').'</th><th class="right">'.$langs->trans('BankSyncTransactionCountShort').'</th><th class="right">'.$langs->trans('Action').'</th></tr>';
 if (empty($observed)) {
-    print '<tr><td colspan="5"><span class="opacitymedium">'.$langs->trans('BankSyncNoObservedTaxAccounts').'</span></td></tr>';
+    print '<tr><td colspan="6"><span class="opacitymedium">'.$langs->trans('BankSyncNoObservedTaxAccounts').'</span></td></tr>';
 } else {
     foreach ($observed as $row) {
-        $prefillUrl = $_SERVER['PHP_SELF'].'?prefill_account='.rawurlencode((string) $row->counterparty_account).'&prefill_label='.rawurlencode((string) $row->counterparty_name);
+        $kind = !empty($row->suggested_kind) ? (string) $row->suggested_kind : 'social_contribution';
+        $prefillUrl = $_SERVER['PHP_SELF'].'?prefill_account='.rawurlencode((string) $row->counterparty_account).'&prefill_label='.rawurlencode((string) $row->counterparty_name).'&prefill_kind='.rawurlencode($kind);
+        if ($kind === 'vat') $prefillUrl .= '#vat-mapping';
+        $kindLabel = $kind === 'vat' ? $langs->trans('BankSyncTarget_vat') : $langs->trans('BankSyncTarget_social_contribution');
         print '<tr class="oddeven"><td>'.dol_escape_htmltag((string) $row->counterparty_account).'</td>';
         print '<td>'.dol_escape_htmltag((string) $row->counterparty_name).'</td>';
+        print '<td>'.dol_escape_htmltag($kindLabel).'</td>';
         print '<td>'.dol_escape_htmltag((string) $row->last_date).'</td>';
         print '<td class="right">'.((int) $row->tx_count).'</td>';
         print '<td class="right"><a class="button" href="'.dol_escape_htmltag($prefillUrl).'">'.$langs->trans('BankSyncUseForMapping').'</a></td></tr>';
